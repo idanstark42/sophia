2997f6ef83ed00d9648ec04adf8fb2f118f509c3
Idan Stark <idanstark42@gmail.com>
2024-02-01 03:57:48 +0200

    fixed env var name

--- src/data/conversation.js
+++ src/data/conversation.js
@@ -1,7 +1,7 @@
 const Realm = require('realm')
 const Whatsapp = require('../whatsapp')
 
-const RELAM_APP_ID = process.env.REALM_APP_ID
+const REALM_APP_ID = process.env.REALM_APP_ID
 const REALM_CLUSTER = process.env.REALM_CLUSTER
 
 class Conversation {
@@ -17,12 +17,12 @@ class Conversation {
     await whatsapp.send(output)
     const database = await init()
     const conversations = database.collection('Conversations')
-    await conversations.updateOne({ number: this.number }, { $push: { messages: { $each: [{ _id: generateId(), role: 'user', content: input }, { _id: generateId(), role: 'assistant', content: output }] } } })
+    await conversations.updateOne({ number: this.number }, { $push: { messages: { $each: [{ role: 'user', content: input }, { role: 'assistant', content: output }] } } })
   }
 }
 
 const init = async () => {
-  const app = new Realm.App({ id: RELAM_APP_ID })
+  const app = new Realm.App({ id: REALM_APP_ID })
   const credentials = Realm.Credentials.anonymous()
   await app.logIn(credentials)
   return app.currentUser.mongoClient(REALM_CLUSTER).db('Sophia')
