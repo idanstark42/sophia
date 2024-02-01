fc0df8a3fd05d4eadafec6dab7f18ff6ebb91832
Idan Stark <idanstark42@gmail.com>
2024-02-01 03:43:32 +0200

    using mogno client

--- src/data/conversation.js
+++ src/data/conversation.js
@@ -2,58 +2,30 @@ const Realm = require('realm')
 const Whatsapp = require('../whatsapp')
 
 const RELAM_APP_ID = process.env.REALM_APP_ID
+const REALM_CLUSTER = process.env.REALM_CLUSTER
 
-const getRealm = () => new Realm({ id: RELAM_APP_ID, schema: [Conversation, Message] })
-const generateId = () => Math.floor(Math.random() * 1000000000).toString()
-
-class Conversation extends Realm.Object {
-  static schema = {
-    name: 'Conversation',
-    primaryKey: 'number',
-    properties: {
-      number: 'string',
-      messages: { type: 'list', objectType: 'Message' }
-    },
+class Conversation {
+  static async get (number) {
+    const database = await init()
+    const conversations = database.collection('Conversations')
+    const conversation = await conversations.findOne({ number })
+    return conversation || await conversations.insertOne({ number, messages: [] })
   }
 
-  static get (number) {
-    let conversation
-    const realm = getRealm()
-    realm.write(() => {
-      const conversations = realm.objects('Conversation').filtered(`number = "${number}"`)
-      console.log('conversations')
-      console.log(conversations)
-      conversation = conversations.length ? conversations[0] : realm.create('Conversation', { number, messages: [] })
-      console.log('conversation')
-      console.log(conversation)
-    })
-    realm.close()
-    return conversation
+  async respond (input, output) {
+    const whatsapp = new Whatsapp(this.number)
+    await whatsapp.send(output)
+    const database = await init()
+    const conversations = database.collection('Conversations')
+    await conversations.updateOne({ number: this.number }, { $push: { messages: { $each: [{ _id: generateId(), role: 'user', content: input }, { _id: generateId(), role: 'assistant', content: output }] } } })
   }
+}
 
-  respond (input, output) {
-    const conversation = this
-    const number = this.number
-    const whatsapp = new Whatsapp(number)
-    return whatsapp.send(output).then(() => {
-      const realm = getRealm()
-      realm.write(() => {
-        conversation.messages.push({ _id: generateId(), role: 'user', content: input }, { _id: generateId(), role: 'assistant', content: output })
-      })
-      realm.close()
-    })
-  }
- }
-
-class Message extends Realm.Object {
-  static schema = {
-    name: 'Message',
-    properties: {
-      _id: 'string',
-      content: 'string',
-      role: 'string'
-    }
-  }
+const init = async () => {
+  const app = new Realm.App({ id: RELAM_APP_ID })
+  const credentials = Realm.Credentials.anonymous()
+  await app.logIn(credentials)
+  return app.currentUser.mongoClient(REALM_CLUSTER).db('sophia')
 }
 
 module.exports = Conversation
