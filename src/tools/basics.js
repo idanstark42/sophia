3f31da76faf7df9ee0a5f13f0401c5f48eb5b846
Idan Stark <idanstark42@gmail.com>
2024-02-05 17:59:25 +0200

    now it should work

--- src/tools/basics.js
+++ src/tools/basics.js
@@ -12,15 +12,18 @@ const tools = (_conversation, logger) => [
     function: {
       function: async function diagnose_self () {
         await logger.debug('Diagnosing self.')
+        
         const twentyFourHoursAgo = new Date()
         twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24)
+        
         const logs = {}
         for (const level in LEVELS) {
           logs[level] = await Logger.LogEntry.load({ level, timestamp: { $gte: twentyFourHoursAgo } })
         }
         await logger.debug('logs', logs)
+        
         // grade the health of the assistant from 1 to 100 based on the amount of fatals, errors and warnings
-        const hp = Math.max(Object.entries(LEVELS).reduce((grade, [level, { weight }]) => grade - logs[level] * weight, 100), 0)
+        const hp = Math.max(Object.entries(LEVELS).reduce((grade, [level, { weight }]) => grade - logs[level].length * weight, 100), 0)
         await logger.debug('hp: ' + hp, { hp })
         return hp
       }
