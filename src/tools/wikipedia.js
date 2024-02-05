5736b07e514dafad533edc4555f874bb7c04fd4f
Idan Stark <idanstark42@gmail.com>
2024-02-05 23:44:46 +0200

    giving back actually useful information from wiki

--- src/tools/wikipedia.js
+++ src/tools/wikipedia.js
@@ -4,10 +4,12 @@ const tools = (_conversation, logger) => [
   {
     type: 'function',
     function: {
-      function: async function lookup_in_wikipedia (params) {
+      function: async function get_info_from_wikipedia (params) {
         await logger.debug('Searching wikipedia for ' + params.topic)
         const page = await wiki.page(params.topic)
-        return await page.content()
+        const summary = await page.summary()
+        await logger.debug('Found wikipedia page', { summary: summary.extract, content_urls: summary.content_urls, titles: summary.titles })
+        return summary.extract
       },
       parse: JSON.parse,
       parameters: {
