e04a5dc95420be6874713c685b818c292c6c575e
Idan Stark <idanstark42@gmail.com>
2024-02-05 17:44:46 +0200

    added logs to diagnose_self

--- src/tools/basics.js
+++ src/tools/basics.js
@@ -6,11 +6,12 @@ const LEVELS = {
   fatal: { weight: 20 }
 }
 
-const tools = () => [
+const tools = (_conversation, logger) => [
   {
     type: 'function',
     function: {
       function: async function diagnose_self () {
+        await logger.debug('Diagnosing self.')
         const twentyFourHoursAgo = new Date()
         twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24)
         const logs = await Object.keys(LEVELS).reduce(async (obj, level) => {
@@ -18,7 +19,9 @@ const tools = () => [
           return { ...obj, [level]: logs.length }
         }, {})
         // grade the health of the assistant from 1 to 100 based on the amount of fatals, errors and warnings
-        return Math.max(Object.entries(LEVELS).reduce((grade, [level, { weight }]) => grade - logs[level] * weight, 100), 0)
+        const hp = Math.max(Object.entries(LEVELS).reduce((grade, [level, { weight }]) => grade - logs[level] * weight, 100), 0)
+        await logger.debug('hp: ' + hp, { hp })
+        return hp
       }
     }
   }, {
