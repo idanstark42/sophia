17ba689674d2c0a556c7ec4b734e4a7c2355e27a
Idan Stark <idanstark42@gmail.com>
2024-02-05 17:55:28 +0200

    trying another way

--- src/tools/basics.js
+++ src/tools/basics.js
@@ -14,10 +14,10 @@ const tools = (_conversation, logger) => [
         await logger.debug('Diagnosing self.')
         const twentyFourHoursAgo = new Date()
         twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24)
-        const logs = await Object.keys(LEVELS).reduce(async (obj, level) => {
-          const logs = await Logger.LogEntry.load({ level, timestamp: { $gte: twentyFourHoursAgo } })
-          return { ...obj, [level]: logs.length }
-        }, {})
+        const logs = {}
+        for (const level in LEVELS) {
+          logs[level] = await Logger.LogEntry.load({ level, timestamp: { $gte: twentyFourHoursAgo } })
+        }
         await logger.debug('logs', logs)
         // grade the health of the assistant from 1 to 100 based on the amount of fatals, errors and warnings
         const hp = Math.max(Object.entries(LEVELS).reduce((grade, [level, { weight }]) => grade - logs[level] * weight, 100), 0)
