66e2ec4b934b3077f35c6f53836bd04baf8b2490
Idan Stark <idanstark42@gmail.com>
2024-02-27 23:35:03 +0200

    removed trello for now

--- src/apis/trello.js
+++
@@ -1,8 +0,0 @@
-const Trello = require('node-trello')
-
-const TRELLO_API_KEY = process.env.TRELLO_API_KEY
-const TRELLO_API_TOKEN = process.env.TRELLO_API_TOKEN
-
-const trello = new Trello(TRELLO_API_KEY, TRELLO_API_TOKEN)
-
-module.exports = trello
--- src/tools/trello.js
+++ src/tools/trello.js
@@ -1,5 +1,14 @@
-const trello = require('../apis/trello')
+const Trello = require('node-trello')
+const promisify = require('util').promisify
+
 const { functionTool } = require('../tools/utils')
+
+const TRELLO_API_KEY = process.env.TRELLO_API_KEY
+const TRELLO_API_TOKEN = process.env.TRELLO_API_TOKEN
+
+const trello = new Trello(TRELLO_API_KEY, TRELLO_API_TOKEN)
+const get = promisify(trello.get.bind(trello))
+const post = promisify(trello.post.bind(trello))
 
 const LISTS = {
   ideas: { boardName: 'Long Term Projects', listName: 'IDEAS' },
@@ -10,52 +19,41 @@ const LISTS = {
   halted: { boardName: 'Tactical Tasks', listName: 'HALTED' }
 }
 
-module.exports = (_conversation, _logger) => [
-  functionTool(async function get_tasks_in_list(params) {
-    const { boardName, listName } = LISTS[params.list]
+module.exports = () => []
 
-    const boards = await trello.get(`/1/members/me/boards`)
-    const board = boards.find(board => board.name === boardName)
-    if (!board) return []
+// module.exports = (_conversation, _logger) => [
+//   functionTool(async function read_tasks_in_list(params) {
+//     const { listName, boardName } = LISTS[params.list]
 
-    const lists = await trello.get(`/1/boards/${board.id}/lists`)
-    const list = lists.find(list => list.name === listName)
-    if (!list) return []
+//     const board = await get('/1/members/me/boards')
+//       .then(boards => boards.find(board => board.name === boardName))
 
-    const cards = await trello.get(`/1/lists/${list.id}/cards`)
-    return cards
-  }, { list: { type: 'string', enum: Object.keys(LISTS) } }),
+//     if (!board) return 'Board not found'
 
-  functionTool(async function add_task_to_list(params) {
-    const { boardName, listName } = LISTS[params.list]
+//     const list = await get(`/1/boards/${board.id}/lists`)
+//       .then(lists => lists.find(list => list.name === listName))
 
-    const boards = await trello.get(`/1/members/me/boards`)
-    const board = boards.find(board => board.name === boardName)
-    if (!board) return 'Board not found'
+//     if (!list) return 'List not found'
 
-    const labels = await trello.get(`/1/boards/${board.id}/labels`)
+//     const cards = await get(`/1/lists/${list.id}/cards`)
 
-    // create a new card
-    const card = await trello.post(`/1/cards`, {
-      idList: list.id,
-      name: params.taskName,
-      desc: params.description,
-      due: params.dueDate,
-      idLabels: labels.filter(label => params.labels.includes(label.name)).map(label => label.id),
-    })
+//     for (const card of cards) {
+//       const checklists = await get(`/1/cards/${card.id}/checklists`)
+//       card.checklists = checklists.map(checklist => ({ name: checklist.name, items: checklist.checkItems.map(({ id, name, state }) => ({ id, name, state }))}))
+//     }
 
-    // add checklist items
-    for (const item of params.checklist) {
-      await trello.post(`/1/cards/${card.id}/checklists`, {
-      })
-    }
+//     return cards.map(({ id, name, comments, due, labels, checklists }) => ({ id, name, comments, due, labels: labels.map(label => label.name), checklists }))
+//   }, { list: { type: 'string', enum: Object.keys(LISTS) } }),
+  
+//   functionTool(async function create_task_in_list(params) {
+//   }, { list: { type: 'string', enum: Object.keys(LISTS) }, name: { type: 'string' } }),
 
-  }, {
-    list: { type: 'string', enum: Object.keys(LISTS) },
-    taskName: { type: 'string' },
-    dueDate: { type: 'string' },
-    description: { type: 'string' },
-    checklist: { type: 'array', items: { type: 'string' } },
-    labels: { type: 'array', items: { type: 'string', enum: LABELS } }
-  }),
-]
+//   functionTool(async function move_task_to_list(params) {
+//   }, { id: { type: 'string' }, list: { type: 'string', enum: Object.keys(LISTS) } }),
+
+//   functionTool(async function update_task(params) {
+//   }, { id: { type: 'string' }, name: { type: 'string' }, due: { type: 'string' }, labels: { type: 'array', items: { type: 'string' } }, checklists: { type: 'array', items: { type: 'object', properties: { name: { type: 'string' }, items: { type: 'array', items: { type: 'object', properties: { id: { type: 'string' }, name: { type: 'string' }, state: { type: 'string' } } } } } } } }),
+
+//   functionTool(async function prioritize_tasks_in_list(params) {
+//   }, { list: { type: 'string', enum: Object.keys(LISTS) } })
+// ]
