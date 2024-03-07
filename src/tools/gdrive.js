9cc3d1cf6ca700d13010e83999bca39e338e3af5
Idan Stark <idanstark42@gmail.com>
2024-03-07 02:58:00 +0200

    changed the name to category

--- src/tools/gdrive.js
+++ src/tools/gdrive.js
@@ -22,7 +22,7 @@ const getActions = async () => {
       params: { startDate: { type: 'date' }, endDate: { type: 'date' } },
       postProcessing: events => {
         events.forEach(event => {
-          event.type = COLORS_CODING[event.color]
+          event.category = COLORS_CODING[event.color]
           event.start = new Date(event.start)
           event.end = new Date(event.end)
         })
@@ -30,13 +30,13 @@ const getActions = async () => {
       }
     },
     add_calendar_event: {
-      params: { calendarName: { type: 'string', enum: calendarNames }, title: { type: 'string' }, startDate: { type: 'date' }, endDate: { type: 'date' }, description: { type: 'string' }, allDay: { type: 'boolean' }, type: { type: 'string', enum: Object.values(COLORS_CODING) } },
+      params: { calendarName: { type: 'string', enum: calendarNames }, title: { type: 'string' }, startDate: { type: 'date' }, endDate: { type: 'date' }, description: { type: 'string' }, allDay: { type: 'boolean' }, category: { type: 'string', enum: Object.values(COLORS_CODING) } },
       preProcessing: params => {
         params.start = params.startDate
         params.end = params.endDate
         delete params.startDate
         delete params.endDate
-        params.color = Object.entries(COLORS_CODING).find(([_, type]) => type === params.type)[0]
+        params.color = Object.entries(COLORS_CODING).find(([_, category]) => category === params.category)[0]
         return params
       }
     },
