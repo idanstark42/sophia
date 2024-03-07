c5d496a92d008203aa25cb2f1fbec15ec418ed7d
Idan Stark <idanstark42@gmail.com>
2024-03-07 03:59:31 +0200

    bugfix

--- src/tools/gdrive.js
+++ src/tools/gdrive.js
@@ -1,5 +1,5 @@
 const { action } = require('../apis/google')
-const { functionTool } = require('./_utils')
+const { functionTool, safely } = require('./_utils')
 
 // The meaning of the colors in the calendar
 const COLORS_CODING = {
@@ -14,49 +14,31 @@ const COLORS_CODING = {
   graphite: 'financial'
 }
 
-const getActions = async () => {
+module.exports = async (_conversation, logger) => {
   const calendarNames = await action('get_calendar_names').then(calendars => calendars.map(calendar => calendar.name))
 
-  return {
-    get_calendar_events: {
-      params: { startDate: { type: 'string' }, endDate: { type: 'string' } },
-      postProcessing: events => {
+  return [
+    functionTool(async function get_calendar_events(params) {
+      return await safely(async () => {
+        await logger.debug('Reading events from calendar', params)
+        const events = await action('get_calendar_events', params)
         events.forEach(event => {
           event.category = COLORS_CODING[event.color]
           event.start = new Date(event.start)
           event.end = new Date(event.end)
         })
+        await logger.debug('Events read')
         return events
-      }
-    },
-    add_calendar_event: {
-      params: { calendarName: { type: 'string', enum: calendarNames }, title: { type: 'string' }, startDate: { type: 'string' }, endDate: { type: 'string' }, description: { type: 'string' }, allDay: { type: 'boolean' }, category: { type: 'string', enum: Object.values(COLORS_CODING) } },
-      preProcessing: params => {
-        params.start = params.startDate
-        params.end = params.endDate
-        delete params.startDate
-        delete params.endDate
+      }, logger)
+    }, { startDate: { type: 'string' }, endDate: { type: 'string' } }),
+    functionTool(async function add_calendar_event(params) {
+      return await safely(async () => {
+        await logger.debug('Adding event to calendar', params)
         params.color = Object.entries(COLORS_CODING).find(([_, category]) => category === params.category)[0]
-        return params
-      }
-    },
-  }
-}
-
-module.exports = async (_conversation, logger) => {
-  const actions = await getActions()
-  return Object.entries(actions).map(([actionName, actionDefinition]) => {
-    const func = async function (params) {
-      await logger.debug('Calling google action', { actionName, params })
-      if (actionDefinition.preProcessing) params = actionDefinition.preProcessing(params)
-      const response = await action(actionName, params)
-      await logger.debug('Google action response', { actionName, response })
-      if (actionDefinition.postProcessing) return actionDefinition.postProcessing(response)
-      return response
-    }
-
-    Object.defineProperty(func, 'name', { value: action })
-    
-    return functionTool(func, actionDefinition.params)
-  })
+        await action('add_calendar_event', params)
+        await logger.debug('Event added')
+        return 'Event added'
+      }, logger)
+    }, { calendarName: { type: 'string', enum: calendarNames }, title: { type: 'string' }, startDate: { type: 'string' }, endDate: { type: 'string' }, description: { type: 'string' }, allDay: { type: 'boolean' }, category: { type: 'string', enum: Object.values(COLORS_CODING) } })
+  ]
 }
