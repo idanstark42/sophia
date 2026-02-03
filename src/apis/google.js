8825ccedce69d11aafb3a463d87977c92416e26b
Idan Stark <idanstark42@gmail.com>
2026-02-03 17:54:11 +0200

    restarting with python

--- src/apis/google.js
+++
@@ -1,7 +0,0 @@
-
-const GOOGLE_API_URL = process.env.GOOGLE_API_URL
-
-exports.action = async function (action, params) {
-  const query = { action, ...params }
-  return await fetch(`${GOOGLE_API_URL}?${Object.entries(query).map(([key, value]) => `${key}=${value}`).join('&')}`).then(response => response.json())
-}
